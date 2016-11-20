49ef72b01040780a2ee83a2b125cd26ad1ce538a
Christopho <christopho@solarus-games.org>
2016-11-20 11:19:24 +0100

    Comment

--- data/scripts/hud/hud.lua
+++ data/scripts/hud/hud.lua
@@ -19,7 +19,8 @@ local function initialize_hud_features(game)
     local element_builder = require(element_config.menu_script)
     local element = element_builder:new(game, element_config)
     if element.set_dst_position ~= nil then
-      -- Compatibility with old HUD element scripts.
+      -- Compatibility with old HUD element scripts
+      -- whose new() method don't take a config parameter.
       element:set_dst_position(element_config.x, element_config.y)
     end
     hud.elements[#hud.elements + 1] = element
