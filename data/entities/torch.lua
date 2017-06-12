ac102a622459e97b02065b2be7150caaf5fa47d6
Christopho <christopho@solarus-games.org>
2017-06-12 23:47:04 +0200

    Fix torch script error

--- data/entities/torch.lua
+++ data/entities/torch.lua
@@ -126,6 +126,7 @@ local game_meta = sol.main.get_metatable("game")
 game_meta:register_event("on_map_changed", function(game)
 
   -- TODO the engine should have an event game:on_world_changed().
+  local map = game:get_map()
   local new_world = map:get_world()
   local world_changed = previous_world == nil or
       new_world == nil or
