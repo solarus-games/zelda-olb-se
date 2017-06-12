6c4bc18874e13fd80cefd52f91eb2f095ba089ed
Christopho <christopho@solarus-games.org>
2017-06-12 23:05:50 +0200

    Dungeon 1 enemies

--- data/maps/dungeons/1/1f.lua
+++ data/maps/dungeons/1/1f.lua
@@ -6,3 +6,10 @@ separator_manager:manage_map(map)
 
 local door_manager = require("scripts/maps/door_manager")
 door_manager:manage_map(map)
+
+function map:on_started(destination)
+
+  if destination == from_b1_nw then
+    map:set_doors_open("auto_door_a")
+  end
+end
--- data/maps/dungeons/1/b1.lua
+++ data/maps/dungeons/1/b1.lua
@@ -1,24 +1,17 @@
--- Lua script of map dungeons/1/b1.
--- This script is executed every time the hero enters this map.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation:
--- http://www.solarus-games.org/doc/latest
-
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map becomes is loaded.
-function map:on_started()
+local separator_manager = require("scripts/maps/separator_manager")
+separator_manager:manage_map(map)
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
-end
+local door_manager = require("scripts/maps/door_manager")
+door_manager:manage_map(map)
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
+function map:on_started(destination)
 
+  if destination == from_1f_n then
+    map:set_doors_open("auto_door_c")
+  elseif destination == from_1f_w then
+    map:set_doors_open("auto_door_d")
+  end
 end
