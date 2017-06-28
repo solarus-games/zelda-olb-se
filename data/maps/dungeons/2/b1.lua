c4ae8c4c52b3b3c44baf9f542f70ccf08161c0c6
Christopho <christopho@solarus-games.org>
2017-06-28 12:08:26 +0200

    Dungeon 2 B1

--- data/maps/dungeons/2/b1.lua
+++ data/maps/dungeons/2/b1.lua
@@ -6,3 +6,10 @@ separator_manager:manage_map(map)
 
 local door_manager = require("scripts/maps/door_manager")
 door_manager:manage_map(map)
+
+function map:on_started(destination)
+
+  if destination == from_1f_c then
+    map:set_doors_open("auto_door_h")
+  end
+end
