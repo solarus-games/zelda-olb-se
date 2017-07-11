e5a48709e0a1fbdf12720dd3fa7a4b2e49bc58fa
Christopho <christopho@solarus-games.org>
2017-07-11 22:32:08 +0200

    Pit initial dialog

---
+++ data/maps/out/e4.lua
@@ -0,0 +1,30 @@
+local map = ...
+local game = map:get_game()
+
+function map:on_started(destination)
+
+  if game:get_value("out_e4_pit_first_dialog") then
+    pit:remove()
+  end
+end
+
+function map:on_opening_transition_finished(destination)
+
+  if destination == from_portal_cave then
+    if not game:get_value("out_e4_pit_first_dialog") then
+      game:set_value("out_e4_pit_first_dialog", true)
+      game:start_dialog("out.e4.pit_hello", function()
+        hero:freeze()
+        local movement = sol.movement.create("straight")
+        movement:set_angle(0)
+        movement:set_speed(64)
+        movement:set_max_distance(176)
+        movement:set_ignore_obstacles(true)
+        movement:start(pit, function()
+          pit:remove()
+          hero:unfreeze()
+        end)
+      end)
+    end
+  end
+end
